0a1f78792617ab5a24b6ff8c064a49eaadd8fa0c
tuh37195 <42982587+tuh37195@users.noreply.github.com>
2021-05-31 19:26:28 -0400

    Update main.js
    added code to detect play, pause, resume, stop, clear, and help commands
    
    also added a wrap function to stylize text

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //required packages
+//const { channel } = require('diagnostic_channel');
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 //get auth data
@@ -6,6 +7,9 @@ const { prefix, token } = require('./auth.json');
 
 //get discord client object
 const client = new Discord.Client();
+
+//holds queues for each server 
+const queue = new Map();
 
 //log in to discord with bot token
 client.login(token);
@@ -29,7 +33,7 @@ client.on("warn", function(info){
 
 // When bot is loaded and ready to start working
 client.on("ready", function(){
-	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+	console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`); 
 
   	client.user.setActivity("Just Vibin\'");
 });
@@ -37,4 +41,40 @@ client.on("ready", function(){
 //message sent on server, most of the important stuff starts here
 client.on("message", function(message){
     console.log(`message is created -> ${message}`);
+
+    //if play command
+    if (message.content.toLowerCase().startsWith(`${prefix}play`)){
+        console.log('Play');
+    }
+    //if pause command
+    else if (message.content.toLowerCase().startsWith(`${prefix}pause`)){
+        console.log('Pause');
+    }
+    //if resume command
+    else if (message.content.toLowerCase().startsWith(`${prefix}resume`)){
+        console.log('Resume');
+    }
+    //if stop command
+    else if (message.content.toLowerCase().startsWith(`${prefix}stop`)){
+        console.log('Stop');
+    }
+    //if clear command
+    else if (message.content.toLowerCase().startsWith(`${prefix}clear`)){
+        console.log('Clear');
+    }
+    //if help command
+    else if (message.content.toLowerCase().startsWith(`${prefix}help`)){
+        console.log('HALP ME');
+        helpMe(message);
+    }
 });
+
+
+
+async function helpMe(message){
+    message.channel.send(wrap("No."));
+}
+
+function wrap(text){
+    return "\`\`\`" + text + "\`\`\`"
+}
